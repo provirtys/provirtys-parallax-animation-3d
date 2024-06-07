c6ff8ac7ad20364e963c8b6d41ae9b356071f380
provirtys <provirtys@gmail.com>
2024-06-07 17:06:33 +0500

    first commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const arrows = document.querySelector('.arrows-box')
 
-arrows.addEventListener('click', e => {
+arrows.addEventListener('click', () => {
   const scrollHeightProp = window.getComputedStyle(document.documentElement).getPropertyValue('--scroll-height')
   const scrollHeight = parseInt(scrollHeightProp) * window.innerHeight / 100
   window.scrollTo({ top: scrollHeight, behavior: 'smooth' })
